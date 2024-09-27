7a2254821306c9f5c9ec763bdaf870d53e0e1070
zebal <zeballosperedodylanamin@gmail.com>
2024-09-27 01:50:24 -0400

    "backend"

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hola Mundo!');
 });
 
 const PORT = process.env.PORT || 3000;
